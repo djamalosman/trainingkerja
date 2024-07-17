3d06051645e8d0257d9b11b9f7282d0ec0911a57
Djamal usman86 <djamalusman86@gmail.com>
2024-07-17 10:27:12 +0700

    push

--- resources/views/pages/traningcourse_edit.blade.php
+++ resources/views/pages/traningcourse_edit.blade.php
@@ -512,6 +512,7 @@
             var number_string = angka.replace(/[^,\d]/g, '').toString(),
                 split = number_string.split(','),
                 sisa = split[0].length % 3,
+                rupiah = split[0].substr(0, sisa),
                 ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
             // Tambahkan titik jika yang di input sudah menjadi angka ribuan
