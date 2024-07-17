1d680c01674ce2695581f20f296df7df96cb10d4
Djamal usman86 <djamalusman86@gmail.com>
2024-07-17 11:44:18 +0700

    push

--- resources/views/pages/jobvacancy_edit.blade.php
+++ resources/views/pages/jobvacancy_edit.blade.php
@@ -936,7 +936,7 @@
             timerProgressBar: true,
         });
 
-        var url = "{{ route('/public/edit-traningcourse-detail',':id') }}";
+        var url = "{{ route('edit-traningcourse-detail',':id') }}";
         url = url.replace(":id", id);
         $.ajax({
             url: url,
